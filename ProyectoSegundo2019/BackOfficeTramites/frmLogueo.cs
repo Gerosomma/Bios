f91c4e7362679a2a4a5035cf249c0d9acdd2c424
Emiliano Rogel <emilianorogel6969@gmail.com>
2020-06-25 01:15:56 -0300

    formularios logueo, principal, y solicitud

--- ProyectoSegundo2019/BackOfficeTramites/frmLogueo.cs
+++ ProyectoSegundo2019/BackOfficeTramites/frmLogueo.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BackOfficeTramitesWin.wcfTramite;
+using System.Xml;
+using System.IO;
 
 namespace BackOfficeTramites
 {
     public partial class frmLogueo : Form
     {
+        ServiceClient wcf = new ServiceClient();
         public frmLogueo()
         {
             InitializeComponent();
+            //creo el controlador del evento de logueo del ControlLogueo
+            controlLogueo1.AutenticarUsuario += new EventHandler(VerificoIngreso);
+        }
+
+        public void VerificoIngreso(object sender, EventArgs e)
+        {
+            try
+            {
+                //Empleado _unEmpleado = (Empleado)FabricaLogica.GetLogicaUsuario().LogueoUsuario(controlLogueo1.Usuario, controlLogueo1.Contrasena);
+                Empleado _unEmpleado = (Empleado)wcf.LogueoUsuario(controlLogueo1.Usuario, controlLogueo1.Contrasena);
+
+                if (_unEmpleado == null)
+                    lblError.Text = "CI o Pass Invalidos";
+                else
+                {
+                    XmlDocument doc = new XmlDocument();
+                    XmlElement raiz = doc.CreateElement("EmpleadoLogueado");
+
+                    doc.AppendChild(raiz);
+
+                    XmlElement documento = doc.CreateElement("Documento");
+                    XmlText textoDocumento = doc.CreateTextNode(_unEmpleado.Documento.ToString());
+                    documento.AppendChild(textoDocumento);
+                    raiz.AppendChild(documento);
+
+                    XmlElement pw = doc.CreateElement("Contrasenia");
+                    XmlText textoPw = doc.CreateTextNode(_unEmpleado.Contrasenia);
+                    pw.AppendChild(textoPw);
+                    raiz.AppendChild(pw);
+
+                    XmlElement nombre = doc.CreateElement("Nombre");
+                    XmlText textoNombre = doc.CreateTextNode(_unEmpleado.NombreCompleto);
+                    nombre.AppendChild(textoNombre);
+                    raiz.AppendChild(nombre);
+
+                    XmlElement inicio = doc.CreateElement("Inicio");
+                    XmlText textoInicio = doc.CreateTextNode(_unEmpleado.HoraInicio);
+                    inicio.AppendChild(textoInicio);
+                    raiz.AppendChild(inicio);
+
+                    XmlElement fin = doc.CreateElement("Fin");
+                    XmlText textoFin = doc.CreateTextNode(_unEmpleado.HoraFin);
+                    fin.AppendChild(textoFin);
+                    raiz.AppendChild(fin);
+                    string ruta = Path.Combine(@"XML\empleadoLogueado.xml");
+                    doc.Save(ruta);
+
+                    this.Hide();
+                    Form _unForm = new frmPrincipal(_unEmpleado);
+                    _unForm.ShowDialog();
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+
         }
     }
 }
--- ProyectoSegundo2019/BackOfficeTramites/frmSolicitud.Designer.cs
+++ ProyectoSegundo2019/BackOfficeTramites/frmSolicitud.Designer.cs
@@ -28,11 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnRechazada = new System.Windows.Forms.Button();
+            this.dgvSolicitudes = new System.Windows.Forms.DataGridView();
+            this.BarraEstado = new System.Windows.Forms.StatusStrip();
+            this.LblError = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).BeginInit();
+            this.BarraEstado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(122, 12);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(149, 23);
+            this.btnEjecutar.TabIndex = 71;
+            this.btnEjecutar.Text = "Marcar como ejecutada";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click_1);
+            // 
+            // btnRechazada
+            // 
+            this.btnRechazada.Location = new System.Drawing.Point(277, 12);
+            this.btnRechazada.Name = "btnRechazada";
+            this.btnRechazada.Size = new System.Drawing.Size(152, 23);
+            this.btnRechazada.TabIndex = 72;
+            this.btnRechazada.Text = "Marcar como anulada";
+            this.btnRechazada.UseVisualStyleBackColor = true;
+            this.btnRechazada.Click += new System.EventHandler(this.btnRechazada_Click_1);
+            // 
+            // dgvSolicitudes
+            // 
+            this.dgvSolicitudes.AllowUserToAddRows = false;
+            this.dgvSolicitudes.AllowUserToDeleteRows = false;
+            this.dgvSolicitudes.AllowUserToOrderColumns = true;
+            this.dgvSolicitudes.AllowUserToResizeColumns = false;
+            this.dgvSolicitudes.AllowUserToResizeRows = false;
+            this.dgvSolicitudes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSolicitudes.Location = new System.Drawing.Point(12, 71);
+            this.dgvSolicitudes.Name = "dgvSolicitudes";
+            this.dgvSolicitudes.ReadOnly = true;
+            this.dgvSolicitudes.Size = new System.Drawing.Size(560, 465);
+            this.dgvSolicitudes.TabIndex = 73;
+            // 
+            // BarraEstado
+            // 
+            this.BarraEstado.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LblError});
+            this.BarraEstado.Location = new System.Drawing.Point(0, 539);
+            this.BarraEstado.Name = "BarraEstado";
+            this.BarraEstado.Size = new System.Drawing.Size(584, 22);
+            this.BarraEstado.TabIndex = 74;
+            this.BarraEstado.Text = "statusStrip1";
+            // 
+            // LblError
+            // 
+            this.LblError.Name = "LblError";
+            this.LblError.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmSolicitud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.BarraEstado);
+            this.Controls.Add(this.dgvSolicitudes);
+            this.Controls.Add(this.btnRechazada);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "frmSolicitud";
             this.Text = "frmSolicitud";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).EndInit();
+            this.BarraEstado.ResumeLayout(false);
+            this.BarraEstado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnRechazada;
+        private System.Windows.Forms.DataGridView dgvSolicitudes;
+        private System.Windows.Forms.StatusStrip BarraEstado;
+        private System.Windows.Forms.ToolStripStatusLabel LblError;
     }
 }
